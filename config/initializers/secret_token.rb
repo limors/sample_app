adc3e6a72579c8a36c6344ec46717ac6567e3842
limor <limor@tradertools.com>
2013-07-31 22:26:59 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '49a3c59f97e9ca9e081919800060c4d1171cbe3140dba26638a0cdad3f06de8c1dc2035fd1601849637df436fe2a09016e622dbca86c1742657278e69076186c'
